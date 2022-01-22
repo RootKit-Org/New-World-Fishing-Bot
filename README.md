afc4150cfb8e4de82be79150926fa5c3d9954ac3
Qfc9 <elijahharmon@gmail.com>
2022-01-21 19:51:45 -0500

    Squashed commit of the following:
    commit 4da05e33cd0a1ba4636653db3247664eb06bead7
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Fri Jan 21 15:02:46 2022 -0500
    
        Updated for python error
    
    commit 937d541bdd1c5c6d6ffccd533fbe6c5627101bd4
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Thu Jan 20 15:03:51 2022 -0500
    
        Adding our video on how to install python
    
    commit 521623afd979a5d11f5f0f37880974084806464a
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Mon Jan 10 22:05:50 2022 -0500
    
        PRs Welcome added
    
    commit 15a15324778b840335f17b89d8db2c58027c53ae
    Merge: 2c5c29e 83a21c4
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Sun Jan 9 13:10:03 2022 -0500
    
        Merge pull request #2 from niklastheiler/Add-Auto-Recenter
    
        Added-Auto-Recentering
    
    commit 83a21c41f01200f7e79b7f237775336eb4068d73
    Author: Niklas Theiler <86804552+niklastheiler@users.noreply.github.com>
    Date:   Sun Jan 9 18:51:09 2022 +0100
    
        Update-Add-Auto-Recenter
    
        Changing import from "pyautogui" to "pydirectinput" to avoid changing the default keybindings.
    
    commit 45f3f86e241f676b248e3cb46b1b3686f839d8aa
    Author: Niklas Theiler <86804552+niklastheiler@users.noreply.github.com>
    Date:   Sun Jan 9 17:09:58 2022 +0100
    
        Added auto recentering feature
    
    commit 2c5c29e96d0f7cc3ff830a716ac20614c265b2e0
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Sat Jan 8 22:40:10 2022 -0500
    
        Added GNU GPLv3
    
    commit b47747974d4b1e02e24da3428d35ee1b29681c0a
    Author: Qfc9 <elijahharmon@gmail.com>
    Date:   Sun Jan 2 13:48:56 2022 -0500
    
        Updated so now it will keep pulling till the reel icon turns orange
    
    commit 6be3cc7a2f1d90105d54ecded31038d7869f3540
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Wed Dec 29 22:49:34 2021 -0500
    
        Added instructions on how to download the code
    
    commit 29d35a7c71fc87d44a8c88fa028888f16f97ab73
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Wed Dec 29 20:17:36 2021 -0500
    
        Made the readme more beginner friendly
    
    commit 5918adcdc22c1818b9064dfa52ed6205e15c253f
    Author: Elijah Harmon <elijahharmon@gmail.com>
    Date:   Tue Dec 14 16:54:27 2021 -0500
    
        Added cd
    
    commit fb171fb79d60f831b5b6f415a912bbe60c42939c
    Author: Qfc9 <elijahharmon@gmail.com>
    Date:   Fri Dec 10 23:31:01 2021 -0500
    
        Fixed afk movement and using faster screenshotting library
    
    commit 8f21be1695abec46a594173e26656d1d03df0906
    Merge: d2dfa7f c72be34
    Author: Qfc9 <elijahharmon@gmail.com>
    Date:   Thu Dec 9 15:40:17 2021 -0500
    
        Merge branch 'dev' into main

--- README.md
+++ README.md
@@ -1,28 +1,46 @@
+[![Pull Requests Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com)
 # Basic New World Fishing Bot
 Watch the video! - https://www.youtube.com/watch?v=TCv77s0AXvE
 
-Join teh Discord - https://discord.com/invite/YwMqW5P8ZN
+Join teh Discord - https://discord.gg/rootkit
 
 ### Known Features
 - Works out of the box when being ran at 1920x1080
 
 ### Prereqs
-Make sure you have a pet Python - https://www.python.org/
+1. Download the project by clicking on the green "code" button torward the top right of this repo. A drop down will appear, click the option to 'Download ZIP'
 
-Make sure you pip install the following packages
+2. Unzip the file and place the folder somewhere easy to access
+
+3. Make sure you have a pet Python (aka install python) - https://www.youtube.com/watch?v=N7r8_HVqqy8&ab_channel=RootKit
+
+4. (Windows Users) Open up either `PowerShell` or `Command Promt`. This can be done by pressing the Windows Key and searching for one of those applicaitons.
+
+5. Copy and past the commands below into your terminal. This will install the Open Source packages needed to run the program.
 ```
 pip install PyAutoGUI
 pip install PyDirectInput
 pip install Pillow
 pip install opencv-python
+pip install mss
+pip install numpy
 ```
+***IF YOU GET THE FOLLOWING ERROR `python is not recognized as an internal or external command, operable program, or batch file` Watch This: https://www.youtube.com/watch?v=E2HvWhhAW0g&ab_channel=RootKit***
 
-New World must be installed and running..... But you know this already right?
+5. New World must be installed and running..... But you know this already right?
 
 ### Run
-If you have python and the packages you are good to go. Load up New World on your MAIN monitor (Only applicable for people with multiple monitors), enter the game and walk you favorite fishing spot, set up your rod with bait, and run!
-```
-python main.py
-```
+If you have python and the packages you are good to go. Load up New World on your MAIN monitor (Only applicable for people with multiple monitors), enter the game and walk you favorite fishing spot, set up your rod with bait, and do the following.
+
+1. (Windows Users) Open up either `PowerShell` or `Command Promt`. This can be done by pressing the Windows Key and searching for one of those applicaitons.
+
+2. Type `cd ` (make sure you add the space after the cd or else I will call you a monkey)
+
+3. Drag and drop the folder that has the bot code onto the terminal
+
+4. Press the enter key
+
+5. Type `python main.py`, press enter and that is it!
+
 **We are always looking for new Volunteers to join our Champions!
 If you have any ideas for videos or programs, let us know!**
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pyautogui
 import pydirectinput
 import time
 import random
+import mss
+import numpy as np
+from PIL import Image
 import gc
 
 def main():
@@ -14,16 +17,17 @@ def main():
     # Max random amount of time to add to the base
     castingRandom = .4
 
-    # Base reeling time it will always do
-    reelingBaseTime = .3
-    # Max random amount of time to add to the base
-    reelingRandom = .2
+    # How long to slack the line
+    lineSlackTime = 1.5
 
     # Adding randomness to the wait times for the animations
     animationSleepTime = .1 + (.1 * random.random())
 
     # Randomly will move right or left to keep from AFKing
     moveDirection = ["a", "d"]
+
+    # Free cam key
+    freeCamKey = "alt"
 
     # Finds all Windows with the title "New World"
     newWorldWindows = pyautogui.getWindowsWithTitle("New World")
@@ -48,12 +52,23 @@ def main():
     time.sleep(animationSleepTime)
 
     # Selecting the middle 3rd of the New World Window
-    region = (newWorldWindow.left + round(newWorldWindow.width/3), newWorldWindow.top, round(newWorldWindow.width/3), newWorldWindow.height)
+    mssRegion = {"mon": 1, "top": newWorldWindow.top, "left": newWorldWindow.left + round(newWorldWindow.width/3), "width": round(newWorldWindow.width/3), "height": newWorldWindow.height}
+
+    # Starting screenshotting object
+    sct = mss.mss()
+
+    # This should resolve issues with the first cast being short
+    time.sleep(animationSleepTime * 3)
 
     while True:
+        # Screenshot
+        sctImg = Image.fromarray(np.array(sct.grab(mssRegion)))
         # Calculating those times
         castingTime = castingBaseTime + (castingRandom * random.random())
-        reelingTime = reelingBaseTime + (reelingRandom * random.random())
+
+        # Hold the "Free Look" Button
+        print("Holding Free Look Button")
+        pydirectinput.keyDown(freeCamKey)
 
         # Like it says, casting
         print("Casting Line")
@@ -62,8 +77,10 @@ def main():
         pyautogui.mouseUp()
 
         # Looking for the fish icon, doing forced garbage collection
-        while pyautogui.locateOnScreen("imgs/fishIcon.png", grayscale=True, confidence=.6, region=region) is None:
+        while pyautogui.locate("imgs/fishIcon.png", sctImg, grayscale=True, confidence=.6) is None:
             gc.collect()
+            # Screenshot
+            sctImg = Image.fromarray(np.array(sct.grab(mssRegion)))
 
         # Hooking the fish
         print("Fish Hooked")
@@ -71,25 +88,40 @@ def main():
         time.sleep(animationSleepTime)
 
         # Keeps reeling into "HOLD Cast" text shows on screen
-        while pyautogui.locateOnScreen("imgs/holdCast.png", grayscale=True, confidence=.55, region=region) is None:
+        while pyautogui.locate("imgs/holdCast.png", sctImg, grayscale=True, confidence=.55) is None:
             print("Reeling....")
             pyautogui.mouseDown()
-            time.sleep(reelingTime)
-            pyautogui.mouseUp()
+
+            # If icon is in the orange area slack the line
+            if pyautogui.locate("imgs/fishReelingOrange.png", sctImg, grayscale=True, confidence=.75) is not None:
+                print("Slacking line...")
+                pyautogui.mouseUp()
+                time.sleep(lineSlackTime)
 
             # Uses a lot of memory if you don't force collection
             gc.collect()
+            # Screenshot
+            sctImg = Image.fromarray(np.array(sct.grab(mssRegion)))
 
-            # Uncomment below if you want more randomness in your reeling rest periods
-            # time.sleep(animationSleepTime)
+            # Reel down time
+            time.sleep(animationSleepTime)
 
+        pyautogui.mouseUp()
+        time.sleep(animationSleepTime)
         print("Caught Fish")
 
         # 20% chance to move to avoid AFK timer
         if random.randint(1, 5) == 5:
-            pyautogui.press(moveDirection[random.randint(0, 1)])
+            key = moveDirection[random.randint(0, 1)]
+            pyautogui.keyDown(key)
+            time.sleep(.1)
+            pyautogui.keyUp(key)
 
         time.sleep(animationSleepTime)
+
+        # Release the "Free Look" Button
+        print("Released Free Look Button")
+        pydirectinput.keyUp(freeCamKey)
 
 
 # Runs the main function
